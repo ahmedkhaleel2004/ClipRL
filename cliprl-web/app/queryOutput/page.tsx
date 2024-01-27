0dada9b24c932552528410697276dd20f07ee418
Ahmed Khaleel <ahmedkhaleel2004@gmail.com>
2024-01-27 14:58:10 -0500

    more build issues

--- cliprl-web/app/queryOutput/page.tsx
+++ cliprl-web/app/queryOutput/page.tsx
@@ -3,7 +3,7 @@ import { queryData } from "@/lib/getQueryOutput";
 const Query = async () => {
 	const queryOutput = await queryData();
 	console.log(JSON.stringify(queryOutput, null, 2));
-	return <div>Query</div>;
+	return <div>Query test</div>;
 };
 
 export default Query;
