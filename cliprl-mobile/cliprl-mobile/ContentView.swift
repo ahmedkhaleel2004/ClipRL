1722555060bb0afc566f787b4fd7b85fa6be6dc9
Shaaf <shaaf.m.shahzad@gmail.com>
2024-01-27 13:26:20 -0500

    Initialized xcode environment

---
+++ cliprl-mobile/cliprl-mobile/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  cliprl-mobile
+//
+//  Created by Shaaf Shahzad on 2024-01-27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ cliprl-mobile/cliprl-mobile/cliprl_mobileApp.swift
@@ -0,0 +1,17 @@
+//
+//  cliprl_mobileApp.swift
+//  cliprl-mobile
+//
+//  Created by Shaaf Shahzad on 2024-01-27.
+//
+
+import SwiftUI
+
+@main
+struct cliprl_mobileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
