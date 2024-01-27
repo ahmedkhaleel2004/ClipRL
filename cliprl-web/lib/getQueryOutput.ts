d3fa5397b530b898ff80b77f0d6bba6d6f0d851f
Ahmed Khaleel <ahmedkhaleel2004@gmail.com>
2024-01-27 14:23:39 -0500

    build issues

--- cliprl-web/lib/getQueryOutput.ts
+++ cliprl-web/lib/getQueryOutput.ts
@@ -1,6 +1,9 @@
 import prisma from "./prismaClient";
 
 export const queryData = async () => {
+	if (!prisma) {
+		throw new Error("Prisma client is not initialized");
+	}
 	try {
 		const data = await prisma.users.findMany();
 		return data;
