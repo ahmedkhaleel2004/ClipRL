ed9bc7be86496a5bd39fbe33ce9c4efd1a5b4ab9
Omar Ramadan <oramadan2004@gmail.com>
2024-01-27 14:18:23 -0500

    main page

--- cliprl-web/app/page.tsx
+++ cliprl-web/app/page.tsx
@@ -1,10 +1,15 @@
 import { ModeToggle } from "@/components/component/ModeToggle";
+import Book from "@/components/component/Book";
+import { pages } from "next/dist/build/templates/app-page";
 
 export default function Home() {
+	const pages = ["Hello Wo", "Hello World", "Hello World!", "epic"];
+
 	return (
 		<main>
 			<ModeToggle />
 			<p className="text-2xl">Hello World</p>
+			<Book pages={pages}></Book>
 		</main>
 	);
 }
---
+++ cliprl-web/components/component/Book.tsx
@@ -0,0 +1,61 @@
+"use client";
+
+import React, { useState } from "react";
+import {
+	Card,
+	CardContent,
+	CardDescription,
+	CardFooter,
+	CardHeader,
+	CardTitle,
+} from "@/components/ui/card";
+
+import { useSwipeable } from "react-swipeable";
+
+import { Button } from "../ui/button";
+import { Car } from "lucide-react";
+
+interface BookProps {
+	pages: string[];
+	// this could change to a more complex type
+}
+
+const Book: React.FC<BookProps> = ({ pages }) => {
+	const [currentPage, setCurrentPage] = useState(0);
+
+	const handlers = useSwipeable({
+		onSwipedLeft: () =>
+			setCurrentPage((prev) => Math.min(prev + 1, pages.length - 1)),
+		onSwipedRight: () => setCurrentPage((prev) => Math.max(prev - 1, 0)),
+		// Remove the preventDefaultTouchmoveEvent property
+		trackMouse: true,
+	});
+
+	return (
+		<Card className="w-full h-full">
+			<CardHeader>
+				<CardTitle>Book</CardTitle>
+				<CardDescription>Book description</CardDescription>
+			</CardHeader>
+			<div
+				{...handlers}
+				className="flex justify-center items-center h-screen"
+			>
+				<div className="relative w-64 h-96">
+					{pages.map((page, index) => (
+						<div
+							key={index}
+							className={`absolute w-full h-full bg-black border border-gray-300 p-4 ${
+								index === currentPage ? "block" : "hidden"
+							}`}
+						>
+							{page}
+						</div>
+					))}
+				</div>
+			</div>
+		</Card>
+	);
+};
+
+export default Book;
