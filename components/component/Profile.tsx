659ee1ed294952f8aa93c7bf5c37f9f39d991f84
Ahmed Khaleel <ahmedkhaleel2004@gmail.com>
2024-01-27 20:41:31 -0500

    test 2

--- components/component/Profile.tsx
+++ components/component/Profile.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { useUser } from "@auth0/nextjs-auth0/client";
-import Image from "next/image";
 
 export default function ProfileClient() {
 	const { user, error, isLoading } = useUser();
@@ -9,17 +8,11 @@ export default function ProfileClient() {
 	if (isLoading) return <div>Loading...</div>;
 	if (error) return <div>{error.message}</div>;
 
-	console.log("user");
-	console.log(user);
-
 	return (
 		user && (
 			<div>
-				aw.fnakwefnajnihiui
-				<Image src={user.picture || ""} alt={user.name || ""} />
 				<h2>Name: {user.name}</h2>awpofjaweoifjoi
 				<p>Email: {user.email}</p>
-				<p> {user.org_id} </p>
 			</div>
 		)
 	);
