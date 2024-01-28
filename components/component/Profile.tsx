fb9d77a50df9c9b52b55201146c9a90d83cb8ce7
Ahmed Khaleel <ahmedkhaleel2004@gmail.com>
2024-01-27 20:37:14 -0500

    test

--- components/component/Profile.tsx
+++ components/component/Profile.tsx
@@ -9,12 +9,17 @@ export default function ProfileClient() {
 	if (isLoading) return <div>Loading...</div>;
 	if (error) return <div>{error.message}</div>;
 
+	console.log("user");
+	console.log(user);
+
 	return (
 		user && (
 			<div>
+				aw.fnakwefnajnihiui
 				<Image src={user.picture || ""} alt={user.name || ""} />
-				<h2>Name: {user.name}</h2>
+				<h2>Name: {user.name}</h2>awpofjaweoifjoi
 				<p>Email: {user.email}</p>
+				<p> {user.org_id} </p>
 			</div>
 		)
 	);
